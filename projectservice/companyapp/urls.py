d720b54a7f33353a50845b3dbe3372da457312af
thanseerthansi <thanseermedammal720@gmail.com>
2022-06-23 11:01:27 +0530

    'firstcommit'

---
+++ projectservice/companyapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+   path('company/',Companyview.as_view()),
+   path('partnerregister/',PartnerRegister.as_view()),
+   path('partnerservice/',PartnerServiceView.as_view()),
+   path('notification/',NotificationView.as_view()),
+   # path('acceptedquote/',Ac.as_view()),
+  
+]
