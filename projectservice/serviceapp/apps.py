4d039e9070b9fad8edb941e098dec55a28b0c1df
thanseerthansi <thanseermedammal720@gmail.com>
2022-06-24 17:41:47 +0530

    file add gitignore

--- projectservice/serviceapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ServiceappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'serviceapp'
--- projectservice/serviceapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-   path('servicetype/',ServiceTypeVew.as_view()),
-   path('servicecity/',ServicecitiesVew.as_view()),
-   path('service/',ServiceVew.as_view()),
-  
-]
