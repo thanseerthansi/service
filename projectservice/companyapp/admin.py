ad8ec945a96cbd08c465d53b9d943acf7d50338b
thanseerthansi <thanseermedammal720@gmail.com>
2022-06-25 10:59:07 +0530

    addedd

--- projectservice/companyapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from companyapp.models import *
-
-# Register your models here.
-admin.site.register(CompanyModel)
-admin.site.register(PartnerServiceModel)
-admin.site.register(NotificationModel)
-admin.site.register(AcceptedQuoteModel)
--- projectservice/serviceapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from serviceapp.models import *
-
-# Register your models here.
-admin.site.register(ServiceTypeModel)
-admin.site.register(ServiceCitiesModel)
-admin.site.register(ServiceModel)
